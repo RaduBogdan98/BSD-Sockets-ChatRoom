e5386da2e911ff4a592a4239da5d241df31a199a
DenisDaniel707 <48672958+DenisDaniel707@users.noreply.github.com>
2020-03-05 01:39:59 +0200

    Update client.c
    Am adaugat functionalitate de username

--- client.c
+++ client.c
@@ -7,20 +7,36 @@
 #include <string.h>
 #include <inttypes.h>
 #include <arpa/inet.h>
+#define IP "192.168.0.106"
+
+char username[256];
 
 int send_to_server(int network_socket) {
-	char message[256] = "";
 
+	char message[256] = "";
+	// printf("%s: ", id);
 	fgets(message, 256, stdin);
 	message[strlen(message) - 1] = 0;
+
 	if (strcmp(message, "End") == 0) {
 		return 0;
 	}
 
 	char message_length[256];
 	sprintf(message_length, "%ld", strlen(message));
+	int username_length = strlen(username);
 
 	if (send(network_socket, &message_length, strlen(message_length), 0) < 0) {
+		perror("Send error");
+		exit(1);
+	}
+
+	if (send(network_socket, &username_length, sizeof(int), 0) < 0) {
+		perror("Send error");
+		exit(1);
+	}
+
+	if (send(network_socket, &username, username_length, 0) < 0) {
 		perror("Send error");
 		exit(1);
 	}
@@ -59,7 +75,7 @@ int main() {
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(9002);
-	server_address.sin_addr.s_addr = inet_addr("172.20.10.13");
+	server_address.sin_addr.s_addr = inet_addr(IP);
 
 	//call the connect function
 	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
@@ -68,7 +84,11 @@ int main() {
 		exit(1);
 	}
 
-	//recieve data from server
+	printf("Choose username: ");
+	fgets(username, 256, stdin);
+	username[strlen(username) - 1] = 0;
+
+	//receive data from server
 	char server_response[256];
 	if (recv(network_socket, &server_response, sizeof(server_response), 0) < 0) {
 		perror("Receive error");
